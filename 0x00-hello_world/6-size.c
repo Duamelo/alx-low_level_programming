db4da6ba215762eac555b12b158e731999063e55
Duamelo <david.dosseh@imsp-uac.org>
2022-09-11 10:45:57 -0700

    bug fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long));
-	printf("Size of a char: %ld byte(s)\n", sizeof(float));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
