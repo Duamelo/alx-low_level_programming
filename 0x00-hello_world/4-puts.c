682e2af010c597fefd4f2bdb4f0e57553e7f73ad
Duamelo <david.dosseh@imsp-uac.org>
2022-09-11 09:44:25 -0700

    puts usage

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print some text to output
+ *
+ * Return: 0 as value
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
