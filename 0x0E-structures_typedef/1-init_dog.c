902585e276fa1fb20fe510ab1d12dd4d9ce7548d
Duamelo <david.dosseh@imsp-uac.org>
2022-10-11 04:05:04 -0700

    init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,21 +3,20 @@
 
 
 /**
- * init_dog - function that initialize a variable of type struct dog
- *
+ * init_dog - Entry point
  * @d: variable of type struct dog *
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
  * Description: function that initialize a viable of type struct dog
- *
- * Return: nothing
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - dog structure
  * @name: name's dog
  * @age: dog's age
  * @owner: owner of the dog
- *
- * Description: dog structure
  */
 struct dog
 {
@@ -12,5 +12,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
