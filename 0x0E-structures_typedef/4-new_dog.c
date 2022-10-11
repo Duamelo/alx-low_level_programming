4666cf21eac281815b6881ae8ffd30d4213f570f
Duamelo <david.dosseh@imsp-uac.org>
2022-10-11 06:05:55 -0700

    nw_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,24 +17,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
-	if (dog != NULL)
+	if (dog == NULL)
+		return (NULL);
+	if (name == NULL)
 	{
-		if (name == NULL)
-		{
-			free(dog);
-			free(owner);
-			return (NULL);
-		}
-		if (owner == NULL)
-		{
-			free(dog);
-			free(name);
-			return (NULL);
-		}
-		dog->name = name;
-		dog->age = age;
-		dog->owner = owner;
-		return (dog);
+		free(dog);
+		free(owner);
+		return (NULL);
 	}
-	return (NULL);
+	if (owner == NULL)
+	{
+		free(dog);
+		free(name);
+		return (NULL);
+	}
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
 }
