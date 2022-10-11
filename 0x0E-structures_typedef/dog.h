e2c587244f28fd1978dc622754daeb9cc16ff687
Duamelo <david.dosseh@imsp-uac.org>
2022-10-11 03:06:48 -0700

    dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,13 @@
-struct dog{
+/**
+ * struct dog - dog structure
+ * @name: name's dog
+ * @age: dog's age
+ * @owner: owner of the dog
+ *
+ * Description: dog structure
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
