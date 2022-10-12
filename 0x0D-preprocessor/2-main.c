09b55ed0df54bcc60efe297cc67fe1b549362d50
Duamelo <david.dosseh@imsp-uac.org>
2022-10-12 04:39:54 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Description: program that prints the name of the file it was compiles from
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
