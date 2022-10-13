920914a82dd37825616b322bd5a8a6ae8fe9bdb5
Duamelo <david.dosseh@imsp-uac.org>
2022-10-13 05:13:23 -0700

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - Entry point
+ *
+ * @array: array parameter
+ * @size: number of elements in the array
+ * @cmp: a pointer to the function to be used to compare values
+ *
+ * Description: function that searches for an integer
+ *
+ * Return: the index of the firt element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
