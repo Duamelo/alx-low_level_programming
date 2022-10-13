4cc25bc05ca3abec54b712e79bcb784413ee6a9d
Duamelo <david.dosseh@imsp-uac.org>
2022-10-13 04:42:46 -0700

    aray_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Entry point
+ *
+ * @array: function's pointers array
+ * @size: size of the array param
+ * @action: function's pointer that to be executed
+ *
+ * Description: execute a function given as a param on each elem of an array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (size >= 1 && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
