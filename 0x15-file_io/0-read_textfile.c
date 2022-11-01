7ce62016e81d1f4cb421f488bdfda1494a883418
Duamelo <david.dosseh@imsp-uac.org>
2022-11-01 01:22:07 -0700

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -31,7 +31,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	fd = open("Requiescat", O_RDWR);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
