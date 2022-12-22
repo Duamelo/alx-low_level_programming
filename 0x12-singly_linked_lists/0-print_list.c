7f780e54777f6553a10f3ea1d452cba9ddb262f1
David DOSSEH <david.dosseh@imsp-uac.org>
2022-12-22 23:30:14 +0100

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,35 +4,34 @@
 
 
 /**
- * print_list: entry point
+ * print_list - entry point
  * @h: head of linked list
  *
  * Description: function that prints all the elements of a list_t list
- *
  * Return: the number of node
  */
 size_t print_list(const list_t *h)
 {
-  size_t size = 0;
-  const list_t *temp;
+	size_t size = 0;
+	const list_t *temp;
 
-  if (h == NULL)
-    exit(0);
-  temp = h;
+	if (h == NULL)
+		exit(0);
+	temp = h;
 
-  while (temp != NULL)
-    {
-      if (temp->str == NULL)
+	while (temp != NULL)
 	{
-	  printf("[0] (nil)\n");
-	  size++;
+		if (temp->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			size++;
+		}
+		else
+		{
+			printf("[%d] %s\n", temp->len,  temp->str);
+			size++;
+		}
+		temp = temp->next;
 	}
-      else
-	{
-	  printf("[%d] %s\n", temp->len,  temp->str);
-	  size++;
-	}
-      temp = temp->next;
-    }
-  return (size);
+	return (size);
 }
